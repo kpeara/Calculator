ad1ed2c96b8421ce67b692ec45538924d38524e9
Kunju Perath <kperath@gmail.com>
2019-12-26 23:36:03 -0500

    initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,31 @@
+public class Calculator {
+
+    private static Calculator instance;
+
+    private int add() {return 0;}
+    private int subtract() {return 0;}
+    private int multiply() {return 0;}
+    private int divide() {return 0;}
+    private int power() {return 0;}
+
+    // utilizing tree data structure
+    // parse brackets
+        // search for brackets if found, look for closing, if not found generate error (create own exception?)
+    // consider invalid input
+    // consider saving history (database...)
+    public String calculate(String parse) {
+        // algorithm
+        // evaluate highest precedence first, store those in tree structure
+        return parse;
+    }
+
+    // singleton
+    private Calculator() {}
+    public static Calculator getCalculator() {
+        if (instance == null) {
+            instance = new Calculator();
+        }
+        return instance;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator c = Calculator.getCalculator();
+        c.calculate("1 + 2"); // will be window in gui to enter input
+    }
+}
