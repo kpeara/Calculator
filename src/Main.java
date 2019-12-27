da1b344729d3a004de81eae8530daaea8d2729cf
Kunju Perath <kperath@gmail.com>
2019-12-26 23:44:19 -0500

    git sync with intellij

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         Calculator c = Calculator.getCalculator();
-        c.calculate("1 + 2"); // will be window in gui to enter input
+        c.calculate("1 + 3"); // will be window in gui to enter input
     }
 }
